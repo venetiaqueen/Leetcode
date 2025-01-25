672afe4ef4dabcce176d30ebaceb7f3d12b6f1af
Faith Howell <54634193+venetiaqueen@users.noreply.github.com>
2025-01-25 00:15:26 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (28.06%) - LeetSync

---
+++ 2243-check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def checkString(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if "ba" in s:
+            return False
+        else:
+            return True
+        
