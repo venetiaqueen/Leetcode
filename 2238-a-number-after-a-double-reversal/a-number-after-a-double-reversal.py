ff1278a305b3269bbf2316c2886049fc91684633
Faith Howell <54634193+venetiaqueen@users.noreply.github.com>
2025-01-26 23:13:11 -0400

    Time: 0 ms (100.00%) | Memory: 12.4 MB (18.37%) - LeetSync

---
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isSameAfterReversals(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num == 0:
+            return True
+
+        if num % 10 == 0:
+            return False
+        else:
+            return True
